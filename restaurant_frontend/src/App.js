88fb43409de6aced40cffd7a8b91f0ce80da0c85
paulraucci <54955537+paulraucci@users.noreply.github.com>
2019-12-12 16:12:15 -0500

    front and back end up, seeded db

--- restaurant_frontend/src/App.js
+++ restaurant_frontend/src/App.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import Cities from "./Components/Cities";
 
-function App() {
-  return <div className="App"></div>;
+let baseURL = process.env.REACT_APP_BASEURL;
+
+//alternate baseURL = 'https://fathomless-sierra-68956.herokuapp.com'
+
+if (process.env.NODE_ENV === "development") {
+  baseURL = "http://localhost:3001";
+} else {
+  baseURL = "https://developers.zomato.com/api/v2.1/";
+}
+
+console.log("current base URL:", baseURL);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Cities />
+      </div>
+    );
+  }
 }
 
 export default App;
--- restaurant_frontend/src/Components/Cities.js
+++ restaurant_frontend/src/Components/Cities.js
@@ -1,23 +1,40 @@
 import React, { Component } from "react";
+// import axios from "axios";
 
 class Cities extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cities: []
+      cities: [],
+      city: {}
     };
   }
-
-  async componentDidMount() {
-    const response = await axios(url);
-    const data = response.data;
-    this.setState({
-        title: data.title
-    })
+  componentDidMount() {
+    this.getCities();
   }
-
+  getCities() {
+    fetch("http://localhost:3000/cities")
+      .then(response => response.json())
+      .then(json => this.setState({ cities: json }))
+      .catch(error => console.error(error));
+  }
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <main>
+          <h1>
+            {this.state.cities.map(city => {
+              return (
+                <div>
+                  <h1>{city.city}</h1>
+                  {/* <h2>{city.location}</h2> */}
+                </div>
+              );
+            })}
+          </h1>
+        </main>
+      </div>
+    );
   }
 }
 
